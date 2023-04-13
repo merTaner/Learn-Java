208b5ea1f9d1a858df7def93b9ec5bd2da0350f9
Mert Taner <merttaner44@gmail.com>
2023-04-13 22:34:07 +0300

    completed first step

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Java is a case sensitive language
+		// in Java use camelCase
+		// Reusability -> this meaning one varible reuse 
+
+	}
+
+}
